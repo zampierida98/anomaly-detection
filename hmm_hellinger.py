327df07cf320afe218f0447fa7eaa267dd240604
dallachiaram <dallachiaram@gmail.com>
2021-01-11 20:38:21 +0100

    aggiunto pca rolling avg

--- hmm_hellinger.py
+++ hmm_hellinger.py
@@ -9,6 +9,9 @@ import numpy as np
 from scipy import stats, linalg
 from hmmlearn import hmm
 import matplotlib.pyplot as plt
+import os
+
+from sklearn.decomposition import PCA
 
 
 # COV_DATA: set of the probability distributions over observations in each state
@@ -24,7 +27,7 @@ def hellinger_dist(mu_model, cov_model, mu_data, cov_data):
 # viene calcolato uno score ad ogni sliding window.
 # Quando supera lo threshold è una anomalia.
 
-# Online Anomaly Detection
+# Viterbi training
 def evaluate(model, data, w):
     i = w
     scores = []
@@ -32,8 +35,8 @@ def evaluate(model, data, w):
         # finestra all'istante t-esimo della ts data[0:t]->data[1:t+1]->data[2:t+2]
         Wt = data[i-w:i].copy()
         
-        # Find most likely state sequence corresponding to Wt
-        # ll: probabilità log della seq di stati prodotta
+        # utilizza l'algoritmo di Viterbi per trovare il Viterbi path (la sequenza di stati pi`u probabile) basandosi sul modello corrente
+        # ll: log probabilità della seq di stati prodotta
         # St: seq di stati prodotta. Stessa len di Wt ovvimente
         ll, St = model.decode(Wt)
         
@@ -60,31 +63,108 @@ def evaluate(model, data, w):
     return scores
 
 ##########PARAMETERS############################
-path_data = './datasets/ALFA_INTERPOLATED/carbonZ_2018-07-18-15-53-31_1_engine_failure.csv'
+path_data = './datasets/ALFA_INTERPOLATED/'
 w = 100
 K = 5
 ###############################################
 
 ##########DATA AND MODEL TRAINING##############
-dataset = pd.read_csv(path_data).values
-# posizione originale
+
+dataset = []
+for file in os.listdir(path_data):
+    dataset.append(pd.read_csv(path_data + file).values)
+
+
+# viene stabilito il modello
 model = hmm.GaussianHMM(n_components=K, covariance_type="diag", n_iter=100, random_state=0)
 
 ########### NOSTRA PARTE
-dataset = np.delete(dataset, np.s_[0], axis=1) #rimuoviamo la colonna indice
-dataset = np.delete(dataset, np.s_[6], axis=1)
 
-# cambio di tipo perchè la funzione np.cov da problemi di riconoscimento di tipo
-dataset = dataset.astype('float32')
+for i in range(len(dataset)):
+    dataset[i] = np.delete(dataset[i], np.s_[0], axis=1) #rimuoviamo la colonna indice
+    dataset[i] = np.delete(dataset[i], np.s_[6], axis=1)
+    
+    # cambio di tipo perchè la funzione np.cov da problemi di riconoscimento di tipo
+    dataset[i] = dataset[i].astype('float32')
+
+
+# %%
+# facciamo il plot dei dati per vedere le componenti delle serie temporali
+
+'''
+index = 0
+for column in training_set.transpose():
+    plt.figure()
+    plt.plot(column)
+    plt.title("Column:" + str(index))
+    plt.show()
+    index += 1
+#'''
+
+# i dati vanno normalizzati rimuovendo il rumore
+
+def moving_average(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w
+
+
+dataset_normalized = []
+for d in dataset:
+
+    dataset_normalized.append([])
+    index_normaliz = len(dataset_normalized) - 1
+
+    for column in d.transpose():
+        dataset_normalized[index_normaliz].append(moving_average(column, 32))
+    
+    dataset_normalized[index_normaliz] = np.array(dataset_normalized[index_normaliz])
+    dataset_normalized[index_normaliz] = dataset_normalized[index_normaliz].transpose()
+
+
+final_dataset = []
+for d in dataset_normalized:
+    
+    pca = PCA(n_components=5)
+    final_dataset.append(pca.fit_transform(d))
+    
+# %%
+'''
+index = 0
+for i in range(dataset[0].transpose().shape[0]):
+    plt.figure()
+    plt.plot(dataset[0].transpose()[i], 'b')
+    plt.plot(dataset_normalized[0].transpose()[i], 'r')
+    plt.title("Column:" + str(index))
+    plt.show()
+    index += 1'''
+
+
+'''
+cut_index = int(len(dataset)*0.9)
+training_set = dataset[0:cut_index]
+test_set = dataset[cut_index:]'''
+
 ########### FINE NOSTRA PARTE
 
+# %%
 
-model.fit(dataset)
+'''
+l = [w] * int(len(dataset_normalized)/w)
+l.append(len(dataset_normalized) % int(len(dataset_normalized)/w))'''
+
+dataset_norm_unique = []
+for d in dataset_normalized:
+    for row in d:
+        dataset_norm_unique.append(row)
+
+dataset_norm_unique = np.array(dataset_norm_unique)
+
+model.fit(final_dataset[0])
 
 ###############################################
 
 ##########EVALUATION OF ANOMALY SCORE##########
-anomaly_scores = evaluate(model, dataset, 100)
+
+anomaly_scores = evaluate(model, final_dataset[0], w)
 
 plt.figure()
 plt.plot(anomaly_scores)
